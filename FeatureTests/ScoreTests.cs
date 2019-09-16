ec28b6638f1601f4e44cfe6f948d4af1d65c5cda
Sam Jessep <samjessep77@gmail.com>
2019-09-16 16:32:51 +1200

    added score test file

---
+++ FeatureTests/ScoreTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sudoku;
+
+namespace FeatureTests
+{
+    [TestClass]
+    class ScoreTests
+    {
+        [TestMethod]
+        public void GetScore()
+        {
+            //check score is higher if if sudoku is completed quicker
+            Game theGame = new Game();
+            //Setup
+            theGame.FromCSV("valid3X3", false);
+            theGame.StartTimer();
+            Thread.Sleep(2000);
+            theGame.StopTimer();
+
+            int actualTime = theGame.timeTaken;
+            int expectedTime = 2;
+
+            Assert.AreEqual(expectedTime, actualTime, "time taken should be 2 seconds");
+        }
+    }
+}
