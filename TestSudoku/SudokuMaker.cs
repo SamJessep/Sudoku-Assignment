b0937d5792ba870013a1b69b0c48ef13b0e49fb5
Sam Jessep <samjessep77@gmail.com>
2019-10-29 15:46:57 +1300

    added confirmation message for when trying to save a sudoku game when it isnt valid

--- TestSudoku/SudokuMaker.cs
+++ TestSudoku/SudokuMaker.cs
@@ -23,25 +23,34 @@ namespace Sudoku
 
         private void Export_Click(object sender, EventArgs e)
         {
-            SaveFileDialog saveFileDialog1 = new SaveFileDialog
+            if (!g.IsPuzzleValidForSaving())
             {
-                Filter = "CSV|*.csv",
-                Title = "Save the sudoku game file"
-            };
-            saveFileDialog1.ShowDialog();
-
-            // If the file name is not an empty string open it for saving.
-            if (saveFileDialog1.FileName != "")
-            {
-                System.IO.FileStream fs =
-                    (System.IO.FileStream)saveFileDialog1.OpenFile();
-                MessageBox.Show("Sudoku Game saved to: " + fs.Name);
-                fs.Close();
-                string gameSettings = g.WriteJsonSettings(GetSudokuSettings());
-                
-                string csvGame = g.ToCSVString(g.numbersArray);
-                g.ToCSV(fs.Name, gameSettings, csvGame, csvGame);
+                var confirmResult = MessageBox.Show("Current sudoku is invalid, are you sure you want to save", "Warning",
+                 MessageBoxButtons.YesNo);
+                if (confirmResult == DialogResult.No)
+                {
+                    return;
+                }
             }
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog
+                {
+                    Filter = "CSV|*.csv",
+                    Title = "Save the sudoku game file"
+                };
+                saveFileDialog1.ShowDialog();
+
+                // If the file name is not an empty string open it for saving.
+                if (saveFileDialog1.FileName != "")
+                {
+                    System.IO.FileStream fs =
+                        (System.IO.FileStream)saveFileDialog1.OpenFile();
+                    MessageBox.Show("Sudoku Game saved to: " + fs.Name);
+                    fs.Close();
+                    string gameSettings = g.WriteJsonSettings(GetSudokuSettings());
+
+                    string csvGame = g.ToCSVString(g.numbersArray);
+                    g.ToCSV(fs.Name, gameSettings, csvGame, csvGame);
+                }
         }
 
         private GameSettings GetSudokuSettings()
